6bfb70078179b766d61c957fdfa0a5dc2d49553d
Есентай Алихан Нуржанулы <@corp.itstep>
2020-01-08 21:33:28 +0600

    Add project files.

---
+++ IPTelephonia/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IPTelephonia
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Call(object sender, RoutedEventArgs e)
+        {
+            UdpClient udpClient = new UdpClient(int.Parse(textBoxPort.Text));
+            udpClient.JoinMulticastGroup(IPAddress.Parse(textBoxIP.Text));
+            udpClient.Close();
+            UdpClient senderUdp = new UdpClient(3231);
+            var buf = new byte[4076];
+            var bytes = System.Text.Encoding.UTF8.GetString(buf);
+            Console.Beep(buf.Length, 5000);
+            senderUdp.Close();
+        }
+    }
+}
